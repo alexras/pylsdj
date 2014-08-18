56c4de583847be5fb16818dcd1ca855fc6007b50
Alex Rasmussen <alexras@trifacta.com>
2014-08-17 20:07:05 -0700

    Make imports a little more intuitive.

---
+++ pylsdj/__init__.py
@@ -0,0 +1,20 @@
+__title__ = 'pylsdj'
+__version__ = '1.1.0'
+__build__ = 0x010100
+__author__ = 'Alex Rasmussen'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2014 Alex Rasmussen'
+
+import bread_spec
+import chain
+import clock
+import consts
+import filepack
+from instrument import Instrument
+from phrase import Phrase
+from project import load_lsdsng, load_srm, Project
+from savfile import SAVFile
+from song import Song, Sequence
+from speech_instrument import Word, SpeechInstrument
+from synth import Synth
+from table import Table
