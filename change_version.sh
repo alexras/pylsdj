e07a751998360bb5fb01f49978a4941aa0799160
Alex Rasmussen <alexras@trifacta.com>
2015-01-11 00:33:29 -0800

    change_version.sh updates copyright year and author.

--- change_version.sh
+++ change_version.sh
@@ -1,5 +1,7 @@
 version=$1
 git_tag="v${version}"
+year=`date +%Y`
+author='Alex Rasmussen'
 
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <version number>"
@@ -13,6 +15,11 @@ sed -i .bak -e "s/release = '.*'/release = '${version}'/g" docs/conf.py
 # Replace version in __init__.py
 sed -i .bak -e "s/__version__ = '.*'/__version__ = '${version}'/g" pylsdj/__init__.py
 sed -i .bak -e "s/version='.*'/version='${version}'/g" setup.py
+
+# Update copyright info
+sed -i .bak -e "s/^copyright = .*/copyright = u'$year, $author'/g" docs/conf.py
+sed -i .bak -e "s/^Copyright (c) .*/Copyright (c) $year $author/g" LICENSE
+sed -i .bak -e "s/^__copyright__ = .*/__copyright__ = 'Copyright $year $author'/g" pylsdj/__init__.py
 
 # Remove backup files
 find . -name "*.bak" -exec rm {} \;
