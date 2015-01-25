3110969c2c5b315c59eee99686091d48943741a2
Alex Rasmussen <alexras@acm.org>
2015-01-24 17:36:14 -0800

    Moving a couple hidden fields to start with 'xx' instead of _.

--- pylsdj/bread_spec.py
+++ pylsdj/bread_spec.py
@@ -139,9 +139,9 @@ pulse_instrument = [
         3: "LR"
     })),
     b.padding(2 * 8),
-    ("_default_instr_byte_1", b.byte, {"str_format": padded_hex(2)}),
+    ("xx_default_instr_byte_1", b.byte, {"str_format": padded_hex(2)}),
     b.padding(3 * 8),
-    ("_default_instr_byte_2", b.byte, {"str_format": padded_hex(2)}),
+    ("xx_default_instr_byte_2", b.byte, {"str_format": padded_hex(2)}),
     b.padding(8)
 ]
 
